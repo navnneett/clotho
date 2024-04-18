95832658a7765edaf3cb107bf1ba2122cae6ed7c
Samantha Yan <yyan30@my.bcit.ca>
2024-04-18 13:13:18 -0700

    Everything is working now. Push all latest version.

---
+++ .history/next.config_20240418123602.js
@@ -0,0 +1,12 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'example.com',
+          port: '',
+          pathname: '/account123/**',
+        },
+      ],
+    },
+}
