aa429a8826d2632e39e3692897acf5cb17684425
Boi San Ly <sanboily.2003@gmail.com>
2024-04-23 12:02:57 -0700

    Adding first layout for setting pgae

---
+++ .history/pages/Setting/Setting_20240423115541.js
@@ -0,0 +1,17 @@
+import styles from '@/styles/Setting.module.css'
+import Navigation from '@/components/Navigation'
+import BottomNavigation from '@/components/BottomNavigation/index.js'
+import { useState } from 'react'
+import Image from "next/image"
+import Link from "next/link"
+
+
+export default function Setting() {
+    return(
+        <>
+            <main className={StyleSheet.main}>
+
+            </main>
+        </>
+    )
+}
---
+++ pages/Setting/Setting.js
@@ -0,0 +1,51 @@
+import styles from '@/styles/Setting.module.css'
+import Navigation from '@/components/Navigation'
+import BottomNavigation from '@/components/BottomNavigation/index.js'
+import { useState } from 'react'
+import Image from "next/image"
+import Link from "next/link"
+
+
+export default function Setting() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false); 
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <>
+            {isMenuOpen && <Navigation toggleMenu={toggleMenu} />}
+            <main className={`${styles.main} ${styles.box}`}>
+                <div className={styles.pageTitle}>
+                    <div onClick={toggleMenu}>
+                        <Image 
+                            src={'/images/menu-burger.png'}
+                            alt="Hamburger menu"
+                            width={50}
+                            height={35}
+                            className={styles.menuIcon}
+                        />
+                    </div>
+                    <h1 className={styles.title}>Setting</h1>
+                    <Image 
+                        src={'/images/user-light.png'}
+                        alt="User icon"
+                        width={35}
+                        height={35}
+                        className={styles.userIcon}
+                    />
+                </div>
+
+                <div></div>
+            </main>
+            <BottomNavigation
+                bgColorCollection="var(--button-highlight-light)"
+                pathColorCollection="var(--white)"
+                borderStyle="60%"
+                IconWidth={90}
+                iconHeight={36}
+            />
+        </>
+    )
+}
